99310ba2a0cfb68a257e1a676894d54ae8b4db4b
syke9p3 <gargarkenth93@gmail.com>
2024-08-26 23:11:24 +0800

    created home page banner

---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,39 @@
+import { Badge } from "@/components/ui/badge"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import LabelBadge from "./LabelBadge"
+
+const labels = ["Age", "Gender", "Race", "Physical", "Religion", "Others"]
+
+
+const Banner = () => {
+    return (
+        <div className="mx-4 space-y-8">
+            <Card className="bg-[#1E1E2E] overflow-hidden relative  text-[#EFF1F5] border shadow-sm flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                <CardContent className="grid md:grid-cols-4 gap-4 z-10">
+                    <div className="col-span-1 space-y-3">
+                        <h1 className="">Multilabel Tagalog Hate Speech Classifier </h1>
+                        <div className="flex gap-2 items-center">
+                            <div className="w-6 h-6 border-2 border-sky-400 rounded-full grid place-items-center"><img src="/assets/ph.png" alt="" className="w-4 h-4" /></div>
+                            <p className="text-sm text-sky-400 ">Tagalog</p>
+                        </div>
+                    </div>
+                    <div className="col-span-3 space-y-4">
+                        <p className="text-xs leading-relaxed max-w-xl opacity-90">Input Tagalog hate speech text in the textbox below. The classifier accepts a text input from 15 characters up to 280 characters. Classify hate speech according to the following categories: </p>
+                        <div className="flex flex-wrap gap-1">
+                            {labels.map((label, i) => (
+                                <LabelBadge key={i} name={label} />
+                            ))}
+                        </div>
+                    </div>
+                </CardContent>
+                <div className="absolute opacity-[5%] bottom-0 right-0">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/012/300/958/small/philippines-flag-free-png.png" alt="" />
+                </div>
+
+            </Card>
+        </div>
+    )
+}
+
+
+export default Banner
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,14 +6,16 @@ import {
 
 import { Input } from "./ui/input"
 import User from "./User"
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="bg-white border-b-2 flex flex-col sm:gap-4 sm:py-4 sm:px-8 sticky">
             <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                <div className="text-2xl font-bold">
-                    <h1>mlthsc.</h1>
-                </div>
+                <Link to="/" className="flex gap-3">
+                    <img src="/assets/mlthsc.png" className="w-8" />
+                    <h1 className="text-2xl font-bold">mlthsc.</h1>
+                </Link>
                 <div className="relative ml-auto flex-1 md:grow-0">
                     <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                     <Input
