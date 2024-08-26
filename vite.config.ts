01cb2783bb12cb4ca622366b3ab148dba2478805
syke9p3 <gargarkenth93@gmail.com>
2024-08-27 02:48:25 +0800

    rechange base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://syke9p3.github.io/mlthsc-react/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
